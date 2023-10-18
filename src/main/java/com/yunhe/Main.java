a56f25552274449a185d521ed2d40e97248442ad
pipijo <pipij782@gmail.com>
2023-10-18 17:04:07 +0800

    第一次提交

---
+++ src/main/java/com/yunhe/Main.java
@@ -0,0 +1,12 @@
+package com.yunhe;
+
+/**
+ * @author 无意
+ * @description 功能描述
+ * @create 2023/10/18/018 16:57
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
